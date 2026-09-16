3d7b6b45c6c6fdf085d3fbf0950dd50a0595b0df
alanedusucesso-blip <alanedusucesso@gmail.com>
2026-09-16 12:05:45 -0300

    Criando projeto no vS

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace meuprimeirocrud.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
